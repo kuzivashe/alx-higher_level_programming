e5a6c4d33d5c34db600f699fe820068b8609350a
Kuzivashe <kuzibaye@gmail.com>
2023-01-26 04:40:01 -0800

    C interview practise question

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * is_palindrome - shows if a singly linked list is a palindrome
+  * @head: double pointer that point to the linked list
+  * Return: 1 if is a palindrome, otherwise is 0
+  */
+int is_palindrome(listint_t **head)
+{
+	int count = 0, pos[9999], j;
+	listint_t *prim = NULL;
+
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	prim = *head;
+	for (; prim; count++, prim = prim->next)
+		pos[count] = prim->n;
+	for (j = 0; j < count; j++, count--)
+	{
+		if (pos[j] != pos[count - 1])
+			return (0);
+	}
+	return (1);
+}
