333a51066abca48fc3b746a4454efc1d54b803c5
Kuzivashe <kuzibaye@gmail.com>
2023-03-03 02:38:36 -0800

    sql intro

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 -- Use hbtn_0c_0 database
 USE hbtn_0c_0;
 -- Change name to utf-8
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table MODIFY name VARCHAR(256);
 -- Change table to utf-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Change database to utf-8
