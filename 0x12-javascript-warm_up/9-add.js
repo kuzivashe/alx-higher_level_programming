a3fdf95fdb66deed3e8315fff46fd0b36b316a37
kuzivashe <kuzibaye@gmail.com>
2023-03-14 02:39:01 +0000

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* create an addition function */
 const input0 = parseInt(process.argv[2]);
-const input1 = parseInt(process.arg[3]);
+const input1 = parseInt(process.argv[3]);
 function add (a, b) {
   let sum = 0;
   sum = a + b;
