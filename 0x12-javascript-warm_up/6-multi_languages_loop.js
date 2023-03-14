62febd079e019d888a93cf377cf01ea266ed5e06
kuzivashe <kuzibaye@gmail.com>
2023-03-14 01:32:22 +0000

    loop to print languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+const array = [firstLine, secondLine, thirdLine];
+let count = 0;
+for (; count < array.length; count++) {
+  console.log(array[count]);
+}
