804888db23bfdcb69c45c0feb7ef99a6288a68e6
kuzivashe <kuzibaye@gmail.com>
2023-03-15 00:19:30 +0000

    rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* create a rectangle class in JS */
-module.export = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
