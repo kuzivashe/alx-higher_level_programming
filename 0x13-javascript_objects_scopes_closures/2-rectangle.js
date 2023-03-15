1266d364147a3f8b8408080ff4c38ddb465d8f2b
kuzivashe <kuzibaye@gmail.com>
2023-03-15 00:32:06 +0000

    empty object, rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* create a Rectanlge class in JS */
-module.exports = new class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-}();
+};
